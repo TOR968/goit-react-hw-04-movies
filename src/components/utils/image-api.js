19825051921bd904a74d71f6d0000718c06f3821
TOR968 <tor968@yahoo.com>
2020-12-22 18:55:30 +0200

    Update image-api.js

--- src/components/utils/image-api.js
+++ src/components/utils/image-api.js
@@ -1,11 +1,13 @@
 import KEY from './key';
 import axios from 'axios';
 
-const get = async (query, page = 1) => {
+const get = async () => {
   const { data } = await axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=10`,
+    `https://api.themoviedb.org/3/movie/550?api_key=${KEY}`,
   );
-  return data.hits;
+  return data;
 };
 
 export default get;
+
+// https://api.themoviedb.org/3/movie/550?api_key=1b548a20e014d0ac89134ac09055bb25
