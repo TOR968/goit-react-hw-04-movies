d832023364eaa938f60074aef3b5fc6e7eb9a56f
TOR968 <tor968@yahoo.com>
2020-12-23 16:54:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Switch, Route } from 'react-router-dom';
+// import { Switch, Route } from 'react-router-dom';
 import AppBar from './components/AppBar/AppBar';
 import Container from './components/Container/Container';
-import HomePage from './views/HomePage/HomePage';
-import MoviesPage from './views/MoviesPage/MoviesPage';
-import MovieDetailsPage from './views/MovieDetailsPage/MovieDetailsPage';
-import Cast from './views/Cast/Cast';
-import Reviews from './views/Reviews/Reviews';
-import NotFoundView from './views/NotFoundView';
+// import HomePage from './views/HomePage/HomePage';
+// import MoviesPage from './views/MoviesPage/MoviesPage';
+// import MovieDetailsPage from './views/MovieDetailsPage/MovieDetailsPage';
+// import Cast from './views/Cast/Cast';
+// import Reviews from './views/Reviews/Reviews';
+// import NotFoundView from './views/NotFoundView';
 
 function App() {
   return (
